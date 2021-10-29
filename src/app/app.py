3994fe4ca7aff8dccd9be53bfc498474db3324d9
Moataz <moataz.harone@gmail.com>
2021-10-30 00:53:42 +0200

    lambda handler_fix

--- src/app/app.py
+++ src/app/app.py
@@ -2,14 +2,15 @@ from flask import Flask , request
 import json
 app = Flask(__name__)
 
-@app.route('/api', methods=['POST','GET'])
-def wm_api():
-    message = 'Welcome to our demo API, here are the details of your request:'
-    output = { 'message' : message  , 'Headers' : dict(request.headers) , 'Method' : str(request.method) , 'Body' : str(request.data,'utf8') }
-    json_output = json.dumps(output, indent = 4)
-    return json_output
+def run_api():
+    @app.route('/api', methods=['POST','GET'])
+    def wm_api():
+        message = 'Welcome to our demo API, here are the details of your request:'
+        output = { 'message' : message  , 'Headers' : dict(request.headers) , 'Method' : str(request.method) , 'Body' : str(request.data,'utf8') }
+        json_output = json.dumps(output, indent = 4)
+        return json_output
+    if __name__ == "__main__":
+        from waitress import serve
+        serve(app, host="0.0.0.0", port=8080)
 
-
-if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+run_api()
